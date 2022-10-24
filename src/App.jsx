4b197e2dc8d49cac0e4452618c35dd171933dac6
Lucas Vale <lucas.sv95@hotmail.com>
2022-10-24 01:52:06 -0300

    feat: first page complete

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import "./App.css";
+//import { BsArrowRight } from "react-icons/bs";
+import AboutPage from "./pages/about";
+import Portfolio from "./pages/portfolio";
+
+function App() {
+  return (
+    <>
+      <AboutPage />
+      <Portfolio />
+    </>
+  );
+}
+
+export default App;
---
+++ src/pages/portfolio/index.jsx
@@ -0,0 +1,27 @@
+import { List, Main, Port } from "./style";
+
+function Portfolio() {
+  return (
+    <>
+      <Main>
+        <Port>
+          <h2>PORTIFÓLIO</h2>
+          <List>
+            <li>Trabalho 1</li>
+            <li>Trabalho 2</li>
+            <li>Trabalho 3</li>
+            <li>Trabalho 4</li>
+            <li>Trabalho 5</li>
+            <li>Trabalho 6</li>
+            <li>Trabalho 7</li>
+            <li>Trabalho 8</li>
+            <li>Trabalho 9</li>
+            <li>Trabalho 10</li>
+          </List>
+        </Port>
+      </Main>
+    </>
+  );
+}
+
+export default Portfolio;
